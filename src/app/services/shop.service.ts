886c8e353ba65c2b5245c8c57763a8c886214ea0
rodrigo286 <rodrigov867@gmail.com>
2020-11-29 23:11:44 -0300

    fix path for netlify build

--- src/app/services/shop.service.ts
+++ src/app/services/shop.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
-import { Category } from '../models/Categorys';
+import { Category } from '../models/categorys';
 import { Product } from '../models/product';
 
 @Injectable({
